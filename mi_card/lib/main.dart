fbfe73e2f978a309b328cb07bae406a550defa9e
João Vitor Retamero <joaovretamero@gmail.com>
2021-12-22 15:50:00 -0300

    Mi Card - Layout, avatar and fonts

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,12 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 80,
+                    backgroundImage: AssetImage('images/joao_vitor_retamero.jpg'),
+                  ),
+                  Text(
+                    'João V. Retamero',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'FLUTTER DEVELOPER',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.teal.shade100,
+                      fontSize: 20,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  ElevatedButton.icon(
+                    icon: Icon(Icons.phone),
+                    label: Text('+55 00 98888-7777'),
+                    onPressed: () {},
+                  ),
+                  ElevatedButton.icon(
+                    icon: Icon(Icons.mail),
+                    label: Text('joao@developer.io'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
