ad9c1a1aeb10a18f83c56dd26cb60ce0a3eef638
João Vitor Retamero <joaovretamero@gmail.com>
2021-12-22 16:22:14 -0300

    Mi Card - Info cards and positioning

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -12,48 +12,65 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Padding(
-            padding: EdgeInsets.only(left: 10.0, right: 10.0),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 80,
-                    backgroundImage: AssetImage('images/joao_vitor_retamero.jpg'),
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage('images/joao_vitor_retamero.jpg'),
+                ),
+                Text(
+                  'João V. Retamero',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Text(
-                    'João V. Retamero',
-                    style: TextStyle(
-                      fontFamily: 'Pacifico',
-                      fontSize: 40,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Text(
-                    'FLUTTER DEVELOPER',
-                    style: TextStyle(
-                      fontFamily: 'Source Sans Pro',
-                      color: Colors.teal.shade100,
-                      fontSize: 20,
-                      letterSpacing: 2.5,
-                      fontWeight: FontWeight.bold,
-                    ),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 150,
+                  child: Divider(
+                    color: Colors.teal.shade100,
                   ),
-                  ElevatedButton.icon(
-                    icon: Icon(Icons.phone),
-                    label: Text('+55 00 98888-7777'),
-                    onPressed: () {},
-                  ),
-                  ElevatedButton.icon(
-                    icon: Icon(Icons.mail),
-                    label: Text('joao@developer.io'),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
+                ),
+                infoButton(Icons.phone, '+55 00 98888-7777'),
+                infoButton(Icons.mail, 'joaoretamero@dev.io'),
+              ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget infoButton(IconData icon, String text) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: ListTile(
+        leading: Icon(
+          icon,
+          color: Colors.teal,
+        ),
+        title: Text(
+          text,
+          style: TextStyle(
+            color: Colors.teal.shade900,
+            fontFamily: 'Source Sans Pro',
+            fontSize: 20,
           ),
         ),
       ),
