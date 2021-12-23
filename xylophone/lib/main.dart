51c34983e216264bae3094c8da4b0be903951006
João Vitor Retamero <joaovretamero@gmail.com>
2021-12-23 10:19:15 -0300

    Xylophone - Playing audio

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,8 +10,44 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: null,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: List.generate(
+            7,
+            (index) => NoteButton(note: index + 1),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NoteButton extends StatelessWidget {
+  final int note;
+  final AudioCache audio = AudioCache();
+
+  String get _fileName => 'note$note.wav';
+
+  NoteButton({
+    Key? key,
+    required this.note,
+  }) : super(key: key) {
+    audio.load(_fileName);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: RawMaterialButton(
+        onPressed: () {
+          audio.play(_fileName);
+        },
+        fillColor: Colors.primaries[(note - 1) * 2],
+      ),
     );
   }
 }
