9bd159c352203a614c715cd0acf5cd7236576bfe
João Vitor Retamero <joaovretamero@gmail.com>
2022-01-13 21:57:04 -0300

    Bitcoin Ticker - Added libraries

--- bitcoin_ticker/lib/main.dart
+++ bitcoin_ticker/lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'price_screen.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(primaryColor: Colors.lightBlue, scaffoldBackgroundColor: Colors.white),
-      home: PriceScreen(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.lightBlue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const PriceScreen(),
     );
   }
 }
