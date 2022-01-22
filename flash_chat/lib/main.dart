12d7f7d671adb5a4bb19341d2132918f43f7ea9e
João Vitor Retamero <joaovretamero@gmail.com>
2022-01-22 14:45:53 -0300

    Flash Chat - Adding Firebase

--- flash_chat/lib/main.dart
+++ flash_chat/lib/main.dart
@@ -1,10 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
-void main() => runApp(const FlashChat());
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   const FlashChat({Key? key}) : super(key: key);
