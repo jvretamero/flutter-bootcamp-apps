a4f1fe7d62c366db19f2dfbb2b94ea5fe7b04078
João Vitor Retamero <joaovretamero@gmail.com>
2022-02-09 18:09:28 -0300

    Todoey - Show indicator when loading tasks

--- todoey/lib/components/tasks_list.dart
+++ todoey/lib/components/tasks_list.dart
@@ -4,38 +4,66 @@ import 'package:todoey/components/task_tile.dart';
 import 'package:todoey/models/task.dart';
 import 'package:todoey/models/tasks_data.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Consumer<TasksData>(
-      builder: (BuildContext context, TasksData taskData, Widget? child) {
-        return ListView.builder(
-          itemCount: taskData.count,
-          itemBuilder: (context, index) {
-            return Dismissible(
-              key: UniqueKey(),
-              direction: DismissDirection.endToStart,
-              background: _dismissibleBackground(),
-              child: TaskTile(task: taskData[index]),
-              onDismissed: (direction) {
-                Task removedTask = taskData.removeTask(index);
+  State<TasksList> createState() => _TasksListState();
+}
 
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text('Removed task "${removedTask.title}"'),
-                    action: SnackBarAction(
-                      label: 'Undo',
-                      onPressed: () {
-                        taskData.addTaskAt(index, removedTask);
-                      },
-                    ),
-                  ),
-                );
-              },
+class _TasksListState extends State<TasksList> {
+  late Future _loadingTaskFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    var tasksData = Provider.of<TasksData>(context, listen: false);
+    _loadingTaskFuture = tasksData.loadTasks();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _loadingTaskFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        return Consumer<TasksData>(
+          builder: (BuildContext context, TasksData taskData, Widget? child) {
+            return _taskListView(taskData);
+          },
+        );
+      },
+    );
+  }
+
+  ListView _taskListView(TasksData taskData) {
+    return ListView.builder(
+      itemCount: taskData.count,
+      itemBuilder: (context, index) {
+        return Dismissible(
+          key: UniqueKey(),
+          direction: DismissDirection.endToStart,
+          background: _dismissibleBackground(),
+          child: TaskTile(task: taskData[index]),
+          onDismissed: (direction) {
+            Task removedTask = taskData.removeTask(index);
+
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Removed task "${removedTask.title}"'),
+                action: SnackBarAction(
+                  label: 'Undo',
+                  onPressed: () {
+                    taskData.addTaskAt(index, removedTask);
+                  },
+                ),
+              ),
             );
           },
         );
